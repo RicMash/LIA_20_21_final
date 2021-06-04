45520dfda7192296ee8e51c37f25010e7b540079
Riccardo Mascello <riccardo.mascello@gmail.com>
2021-06-04 19:51:10 +0200

    Added "on close" feature (need a fix), code design

--- src/gen_graph/src/gen_graph_node.cpp
+++ src/gen_graph/src/gen_graph_node.cpp
@@ -7,6 +7,7 @@
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <apriltag_ros/AprilTagDetectionArray.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <signal.h>
 #include <iostream>
 #include <fstream>
 #include <set>
@@ -16,9 +17,8 @@ std::set<int> detected_tags;
 std::vector<float> cur_pos(3,0);
 //not instantiated so we know the first time must be equal to cur_pos
 std::vector<float> old_pos;
-int old_id=1000;
+int old_id;
 std::ofstream output;
-geometry_msgs::TransformStamped transformStamped;
 
 void odometryCallback_(const nav_msgs::Odometry::ConstPtr msg) {
   tf2::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
@@ -26,40 +26,39 @@ void odometryCallback_(const nav_msgs::Odometry::ConstPtr msg) {
   double roll, pitch, yaw;
   m.getRPY(roll, pitch, yaw);
 
-  cur_pos[0]=msg->pose.pose.position.x;//x
-  cur_pos[1]=msg->pose.pose.position.y;//y
-  cur_pos[2]=yaw;//yaw
+  cur_pos = {(float)msg->pose.pose.position.x, (float)msg->pose.pose.position.y, (float)yaw};
 
   //first time in callback
   if(old_pos.empty()){
     old_pos=cur_pos;
     output << "VERTEX_SE2 " << old_id << " " << cur_pos[0] << " " << cur_pos[1] << " " << cur_pos[2] << "\n";
+    return;
   }
-  else{
-    float distance_position= sqrt(pow(cur_pos[0]-old_pos[0],2)+pow(cur_pos[1]-old_pos[1],2));
-    float distance_orientation= cur_pos[2]-old_pos[2];
-    if(distance_position >= 0.1 || distance_orientation >= 0.5){
-      old_id++;
-      output << "VERTEX_SE2 " << old_id << " " << cur_pos[0] << " " << cur_pos[1] << " " << cur_pos[2] << "\n";
-      output << "EDGE_SE2 " << old_id-1 << " " << old_id << " " << cur_pos[0]-old_pos[0] << " " << cur_pos[1]-old_pos[1] << " " << cur_pos[2]-old_pos[2] << "\n";
-      old_pos=cur_pos;
-    }
+
+  float distance_position= sqrt(pow(cur_pos[0]-old_pos[0],2)+pow(cur_pos[1]-old_pos[1],2));
+  float distance_orientation= cur_pos[2]-old_pos[2];
+  if(distance_position >= 0.1 || distance_orientation >= 0.5){
+    old_id++;
+    output << "VERTEX_SE2 " << old_id << " " << cur_pos[0] << " " << cur_pos[1] << " " << cur_pos[2] << "\n";
+    output << "EDGE_SE2 " << old_id-1 << " " << old_id << " " << cur_pos[0]-old_pos[0] << " " << cur_pos[1]-old_pos[1] << " " << cur_pos[2]-old_pos[2] << "\n";
+    old_pos=cur_pos;
   }
 }
 
 void tagDetectedCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr msg) {
+  geometry_msgs::TransformStamped transformStamped;
   for(int i=0;i<msg->detections.size();i++){
     for(int j=0;j<msg->detections[i].id.size();j++){  
       int current_tag_id = msg->detections[i].id[j];
 
       //must take the position with reference to the old position, not the current, because in output we won't have the current position, just the old one
-      tf2::Vector3 tag_current_view_p = tf2::Vector3(msg->detections[i].pose.pose.pose.position.x, msg->detections[i].pose.pose.pose.position.y, msg->detections[i].pose.pose.pose.position.z);
+      tf2::Vector3 tag_current_view_v = tf2::Vector3(msg->detections[i].pose.pose.pose.position.x, msg->detections[i].pose.pose.pose.position.y, msg->detections[i].pose.pose.pose.position.z);
       tf2::Vector3 translation = tf2::Vector3(cur_pos[0]-old_pos[0], cur_pos[1]-old_pos[1], 0);
       tf2::Quaternion rotation; 
       rotation.setRPY(0,0,cur_pos[2]-old_pos[2]);
       tf2::Transform tf_offset = tf2::Transform(rotation, translation);
       
-      tf2::Vector3 tag_old_view_p = tf_offset * tag_current_view_p;
+      tf2::Vector3 tag_old_view_p = tf_offset * tag_current_view_v;
       
       output << "EDGE_SE2_XY " << old_id << " " << current_tag_id << " " <<  tag_old_view_p.x() << " " << tag_old_view_p.y() << "\n";
       
@@ -90,21 +89,28 @@ void tagDetectedCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr ms
   }
 }
 
+void mySigintHandler(int sig) //exec g2o with results on close
+{
+  char* const argv[] = {"/home/ricmash/d/LIA_20_21_final/result.g2o", nullptr};
+  execvp("/home/ricmash/d/g2o-master/bin/g2o_viewer",argv);
+  ros::shutdown();
+}
+
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "gen_graph_node");
 
   ros::NodeHandle n;
+  old_id=1000;
   output.open("result.g2o");
   ros::Subscriber sub_tag = n.subscribe("tag_detections", 1000, tagDetectedCallback);
   ros::Subscriber sub = n.subscribe("odom", 1000, odometryCallback_);
 
-  tf2_ros::Buffer tfBuffer;
-  tf2_ros::TransformListener tfListener(tfBuffer);
+  signal(SIGINT, mySigintHandler);
 
-  while (n.ok()){
-    ros::spinOnce();
-  }
+  ros::spin();
+
   output.close();
+
   return 0;
 };
